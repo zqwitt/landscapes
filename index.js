4727a8dca00ace02604c58fbc98a447a84076756
zqwitt <zach.witt95@gmail.com>
2018-04-14 19:00:26 -0400

    Added background and mountains generator

--- index.js
+++ index.js
@@ -5,35 +5,38 @@ function preload() {
 }
 
 function setup() {
-    var cv = createCanvas(300, 500);
+    var cv = createCanvas(370, 520);
+    background(255);
     colorMode(HSB, 360, 100, 100, 100);
     const rc = rough.canvas(cv.canvas);
 
-
-    image(img, 0, 0, width, height);
 
     var stripes = 8;
     var widths = [];
     var wLeft = width;
     for (var i = 0; i < stripes; i++) {
-        var c = color(random(0, 40), random(10, 70), random(95, 100), random(40, 50))
+        var c = color(random(0, 30), random(10, 70), random(95, 100), random(40, 50))
         if (i == 0) {
             var w = Math.floor(random() * (width / 4) + 20);
             wLeft -= w;
             widths.push(w);
-            rc.rectangle(5, 5, w - 5, height - 10, {
-                strokeWidth: 2,
-                roughness: 0.3,
+            var s = color(0, 0, 0, random(50, 60));
+            rc.rectangle(15, 15, w - 15, height - 30, {
+                strokeWidth: 3,
+                stroke: s,
+                roughness: 0.4,
                 fill: c,
                 fillStyle: 'solid'
             });
         } else if (i == stripes - 1) {
-            var x = width - wLeft - 5;
+            var x = width - wLeft - 15;
             var w = wLeft;
             widths.push(w);
-            rc.rectangle(x + 5, 5, w - 5, height - 10, {
-                strokeWidth: 2,
-                roughness: 0.3,
+            var s = color(0, 0, 0, random(50, 60));
+            rc.rectangle(x + 15, 15, w - 15, height - 30, {
+                strokeWidth: 3,
+                stroke: s,
+                roughness: 0.4,
                 fill: c,
                 fillStyle: 'solid'
             });
@@ -42,19 +45,107 @@ function setup() {
             var w = Math.floor(random(10, wLeft / 2));
             wLeft -= w;
             widths.push(w);
-            rc.rectangle(x, 5, w, height - 10, {
-                strokeWidth: 2,
-                roughness: 0.3,
+            var s = color(0, 0, 0, random(50, 60));
+            rc.rectangle(x, 15, w, height - 30, {
+                strokeWidth: 3,
+                stroke: s,
+                roughness: 0.4,
                 fill: c,
                 fillStyle: 'solid'
             });
         }
     }
-    console.log(widths[0], widths[1], widths[2], widths[3]);
-    rc.rectangle(5, 5, width - 10, height - 10, {
+
+
+    stroke(color(0, 0, 0, random(60, 80)));
+    beginShape();
+    var currentHeight = random(height - height / 2, height - height / 3);
+    var lastHeight = currentHeight;
+    var seg = 20;
+    strokeWeight(3);
+    fill(random(70, 80), 20, 70);
+    vertex(15, height - 15);
+    vertex(15, currentHeight);
+    for (var i = 1; i < seg; i++) {
+        currentHeight = constrain(lastHeight + random(-5, 5), 0, height - 15);
+        vertex(i * width / seg, currentHeight);
+        lastHeight = currentHeight
+    }
+    vertex(width - 15, currentHeight);
+    vertex(width - 15, height - 15);
+    vertex(15, height - 15);
+
+    endShape();
+
+    beginShape();
+    var currentHeight = random(height - height / 3, height - height / 5);
+    var lastHeight = currentHeight;
+    var seg = 20;
+    strokeWeight(3);
+    fill(random(80, 90), 20, 60);
+    vertex(15, height - 15);
+    vertex(15, currentHeight);
+    for (var i = 1; i < seg; i++) {
+        currentHeight = constrain(lastHeight + random(-5, 5), 0, height - 15);
+        vertex(i * width / seg, currentHeight);
+        lastHeight = currentHeight
+    }
+    vertex(width - 15, currentHeight);
+    vertex(width - 15, height - 51);
+    vertex(15, height - 15);
+
+    endShape();
+
+    beginShape();
+    var currentHeight = random(height - height / 6, height - height / 8);
+    var lastHeight = currentHeight;
+    var seg = 20;
+    strokeWeight(3);
+    fill(random(90, 100), 20, 30);
+    vertex(15, height - 15);
+    vertex(15, currentHeight);
+    for (var i = 1; i < seg; i++) {
+        currentHeight = constrain(lastHeight + random(-5, 5), 0, height - 15);
+        vertex(i * width / seg, currentHeight);
+        lastHeight = currentHeight
+    }
+    vertex(width - 15, currentHeight);
+    vertex(width - 15, height - 15);
+    vertex(15, height - 15);
+
+    endShape();
+
+    var s = color(0, 0, 0, random(50, 75));
+    rc.rectangle(15, 15, width - 30, height - 30, {
         strokeWidth: 3,
+        stroke: s,
         roughness: 0.5,
         fill: 'rgba(0,0,0,0)',
         fillStyle: 'solid'
     });
+
+
+    colorMode(RGB, 255, 255, 255, 255);
+    loadPixels();
+    var increment = 0.4;
+    var yoff = 0;
+    for (var y = 15; y < height - 15; y++) {
+        var xoff = 0;
+        for (var x = 15; x < width - 15; x++) {
+            var index = (x + y * width) * 4;
+            var n = noise(xoff, yoff) * 50;
+            var c = color(pixels[index] - n, pixels[index + 1] - n, pixels[index + 2] - n, pixels[index + 3]);
+
+            set(x, y, c);
+
+            var index = ((x + 2) + y * width) * 4;
+            var c = color(pixels[index] + n, pixels[index + 1] + n, pixels[index + 2] + n, pixels[index + 3]);
+            set(x + 2, y, c);
+            xoff += increment;
+        }
+        yoff += increment;
+    }
+
+    updatePixels();
+    //save('myCanvas.jpg');
 }
